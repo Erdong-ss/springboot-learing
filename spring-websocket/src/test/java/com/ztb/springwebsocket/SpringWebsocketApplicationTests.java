1703b1ed71063953a02f982bb0a7795fdd156181
陈长生 <770348252@qq.com>
2020-05-11 19:05:38 +0800

    Init project

---
+++ spring-websocket/src/test/java/com/ztb/springwebsocket/SpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztb.springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
